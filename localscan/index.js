71d043a0d6bed12122ad0219a8b99e781aae62a2
correabuscar <correabuscar+githubcommits@gmail.com>
2022-11-18 15:17:54 +0100

    trying to see how localhost port scanning works
    from javascript
    
    using code from: https://github.com/wybiral/localscan

---
+++ localscan/index.js
@@ -0,0 +1,35 @@
+// code from: https://github.com/wybiral/localscan as it were at commit: https://github.com/wybiral/localscan/commit/93236fed787da4c63ff4f5df8818d3512572484e
+
+const thread = (start, stop, callback) => {
+    const loop = port => {
+        if (port < stop) {
+            fetch('http://127.0.0.1:' + port, {
+                mode: 'no-cors'
+            }).then(resp => {
+                callback(port);
+                loop(port + 1);
+            }).catch(err => {
+                loop(port + 1);
+            });
+        }
+    };
+    setTimeout(() => loop(start), 0);
+};
+
+const scanRange = (start, stop, thread_count) => {
+    const port_range = stop - start;
+    const thread_range = port_range / thread_count;
+    for (let i = 0; i < thread_count; i++) {
+        const _start = 0 | start + thread_range * i;
+        const _stop = 0 | start + thread_range * (i + 1);
+        thread(_start, _stop, port => {
+            const el = document.createElement('div');
+            el.innerText = 'Server found at :' + port;
+            document.body.appendChild(el);
+        });
+    }
+}
+
+window.onload = () => {
+    scanRange(80, 10000, 100);
+};
